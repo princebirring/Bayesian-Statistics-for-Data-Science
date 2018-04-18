c44eda023e5798264f4e68e42c621bbef9814746
princebirring <princebirring1992@gmail.com>
2018-04-18 20:02:52 +0000

    Flask Restful API

---
+++ flask_api/flask_server.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+import dill as pickle
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def apicall():
+	"""API Call
+	Pandas dataframe (sent as a payload) from API Call
+	"""
+	try:
+		test_json = request.get_json()
+		test = pd.read_json(test_json, orient='records')
+
+	except Exception as e:
+		raise e
+	
+	clf = 'model.pk'
+	
+	if test.empty:
+		return(bad_request())
+	else:
+		#Load the saved model
+		print("Loading the model...")
+		loaded_model = None
+		with open(clf,'rb') as f:
+			loaded_model = pickle.load(f)
+
+		print("The model has been loaded...doing predictions now...")
+		predictions = loaded_model.predict(test)
+		
+		"""Add the predictions as Series to a new pandas dataframe
+								OR
+		   Depending on the use-case, the entire test data appended with the new files
+		"""
+		final_pred = pd.DataFrame(list(predictions))
+		
+		"""We can be as creative in sending the responses.
+		   But we need to send the response codes as well.
+		"""
+		responses = jsonify(predictions=final_pred.to_json(orient="records"))
+		responses.status_code = 200
+
+		return (responses)
+
+
+@app.errorhandler(400)
+def bad_request(error=None):
+	message = {
+			'status': 400,
+			'message': 'Bad Request: ' + request.url + '--> Please check your data payload...',
+	}
+	resp = jsonify(message)
+	resp.status_code = 400
+
+	return resp
+
+if __name__ == "__main__":
+	app.run(port = 8000, debug = True )	
